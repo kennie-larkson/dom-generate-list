7df9eb86aef4783cef8aaccc88a4be06af234e92
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2019-11-23 07:42:41 +0100

    styled list

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 
 let select = document.getElementById.bind(document);
+let create = document.createElement.bind(document);
 let inputValue = select('myText');
 let itemsArr = new Array();
 let p = select('pos');
+//let list = select('item');
+p.style.listStyle = 'none';
+
 //let btn = select('btn');
 //btn.addEventListener('click', insertHTML())
 
@@ -12,7 +16,7 @@ function insertHTML(){
     }else{
         p.insertAdjacentHTML('beforeend', 
         `<li id='item' ><input type="checkbox" id="check">
-        <span>${inputValue.value}</span></li>`);
+        <span>${inputValue.value}</span><span><button type="button">Remove</button></span></li>`);
     }
     itemsArr.push(inputValue.value);
 }
