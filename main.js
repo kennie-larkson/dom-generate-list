7ed8f7c03edf927df8e3b9b6797a0ef3c9353d4c
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2019-11-23 07:11:37 +0100

    fixed bug in listener function

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ let select = document.getElementById.bind(document);
 let inputValue = select('myText');
 let itemsArr = new Array();
 let p = select('pos');
-let btn = select('btn');
-btn.addEventListener('click', insertHTML());
+//let btn = select('btn');
+//btn.addEventListener('click', insertHTML())
 
-function insertHTML(e){
-    if(inputValue.value === ' '){
+function insertHTML(){
+    if(inputValue.value === ''){
         alert('Enter a valid data please!');
     }else{
         p.insertAdjacentHTML('beforeend', 
         `<li id='item' ><input type="checkbox" id="check">
-        <span>${inputValue.value}</span></li>`)
+        <span>${inputValue.value}</span></li>`);
     }
+    itemsArr.push(inputValue.value);
 }
